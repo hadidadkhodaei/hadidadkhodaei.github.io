96c818d641ab2ec36c2fcdcfadbaeab124cc70d2
Hadi <hadidadkhodaei24@gmail.com>
2025-06-16 02:12:46 +0330

    Create file.php

---
+++ file.php
@@ -0,0 +1,30 @@
+<?php
+$uploadDir = 'uploads/';
+
+if (!isset($_GET['file'])) {
+    http_response_code(400);
+    echo 'نام فایل مشخص نشده.';
+    exit;
+}
+
+$filename = basename($_GET['file']);
+$filepath = $uploadDir . $filename;
+
+if (!file_exists($filepath)) {
+    http_response_code(404);
+    echo 'فایل پیدا نشد.';
+    exit;
+}
+
+// تعیین نوع محتوا بر اساس پسوند فایل
+$finfo = finfo_open(FILEINFO_MIME_TYPE);
+$mime = finfo_file($finfo, $filepath);
+finfo_close($finfo);
+
+header('Content-Type: ' . $mime);
+header('Content-Length: ' . filesize($filepath));
+header('Content-Disposition: inline; filename="' . $filename . '"');
+
+readfile($filepath);
+exit;
+?>
